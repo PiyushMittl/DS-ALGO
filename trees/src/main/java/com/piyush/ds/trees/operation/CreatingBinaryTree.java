e490a72d159b5de691bebd869041d6fe47b20717
piyushmittl <piyush.mittal@rediffmail.com>
2019-07-30 22:42:50 +0530

    tree progran added

---
+++ trees/src/main/java/com/piyush/ds/trees/operation/CreatingBinaryTree.java
@@ -0,0 +1,71 @@
+package com.piyush.ds.trees.operation;
+
+/**
+ * 
+ * @author Piyush Mittal
+ *
+ */
+
+public class CreatingBinaryTree {
+
+	public static void main(String[] args) {
+		new CreatingBinaryTree().createTree();
+	}
+	
+	
+	Node node;
+	
+	class Node {
+		int data;
+		Node left;
+		Node right;
+		
+		Node(int data, Node left, Node right) {
+			this.data = data;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	void createTree() {
+		CreatingBinaryTree tree=new CreatingBinaryTree();
+		Node node = new Node(10, null, null);
+		
+		/* following is the tree after above statement 
+			  
+	        10 
+	      /   \ 
+	    null  null     
+	    
+	    */
+		
+		tree.node= node;
+		tree.node.left=new Node(8,null,null);
+		tree.node.left=new Node(12,null,null);
+
+		
+		/* following is the tree after above statement 
+			  
+	        10 
+	      /   \ 
+	     8     12     
+	    
+	    */		
+		
+		tree.node.left.left=new Node(5,null,null);
+		tree.node.left.right=new Node(9,null,null);
+
+		/* following is the tree after above statement 
+			  
+	        10 
+	      /   \ 
+	     8     12     
+	   /   \ 
+	  5     9     
+	
+     */		
+
+		
+	}
+
+}
---
+++ trees/src/main/java/com/piyush/ds/trees/operation/LevelOrderTraversal.java
@@ -0,0 +1,94 @@
+package com.piyush.ds.trees.operation;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 
+ * @author Piyush Mittal
+ *
+ */
+
+public class LevelOrderTraversal {
+
+	public static void main(String[] args) {
+		Node root=new LevelOrderTraversal().createTree();
+		new LevelOrderTraversal().levelOrderTraversal(root);
+	}
+	
+	
+	Node node;
+	
+	class Node {
+		int data;
+		Node left;
+		Node right;
+		
+		Node(int data, Node left, Node right) {
+			this.data = data;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	Node createTree() {
+		Node root=new Node(10, null, null);
+		
+		/* following is the tree after above statement 
+			  
+	        10 
+	      /   \ 
+	    null  null     
+	    
+	    */
+		
+		root.left=new Node(8,null,null);
+		root.right=new Node(12,null,null);
+
+		
+		/* following is the tree after above statement 
+			  
+	        10 
+	      /   \ 
+	     8     12     
+	    
+	    */		
+		
+		root.left.left=new Node(5,null,null);
+		root.left.right=new Node(9,null,null);
+
+		/* following is the tree after above statement 
+			  
+	        10 
+	      /   \ 
+	     8     12     
+	   /   \ 
+	  5     9     
+	
+     */		
+	return root;	
+	}
+	
+	
+	
+	Queue<Node> q=new LinkedList<>();
+	void levelOrderTraversal(Node root) {
+		Node temp=root;
+		q.add(temp);
+
+		while(!q.isEmpty())
+		{
+			Node node=q.poll();
+			System.out.println(node.data);
+
+			
+			if(node.left!=null)
+				q.add(node.left);
+			if(node.right!=null)
+				q.add(node.right);
+			
+		}
+		
+	}
+
+}

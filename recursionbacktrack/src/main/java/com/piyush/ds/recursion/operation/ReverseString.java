946b0b7fd06cde458303f943832a6711cd655674
piyushmittl <piyush.mittal@rediffmail.com>
2019-11-10 14:01:45 +0530

    bst document updated

--- recursionbacktrack/src/main/java/com/piyush/ds/recursion/operation/ReverseString.java
+++ recursionbacktrack/src/main/java/com/piyush/ds/recursion/operation/ReverseString.java
@@ -1,5 +1,7 @@
 package com.piyush.ds.recursion.operation;
 
+import java.util.Arrays;
+
 /**
  * 
  * @author Piyush Mittal
@@ -8,23 +10,58 @@ package com.piyush.ds.recursion.operation;
 public class ReverseString {
 	
 	
-	public static void main(String[] args) {
-		String s1="hello";
-		System.out.println(rev(s1,"",-1));
-	}
+	// Java implementation of the approach 
 
-	public static String rev(String s1,String s2,int index)
-	{
-		if(index==s1.length()-1)
-			return "";
-		else
-			s2=rev(s1,s2,++index);
-		s2=s2+s1.charAt(index);
-		return s2;
-	}
-	
-}
+		// Function to return the lexicographically 
+		// smallest string of length n that 
+		// satisfies the given condition 
+		public static /*char[]*/ String lexo_small(int k, int n) 
+		{ 
+			char arr[] = new char[n]; 
 
+			Arrays.fill(arr, 'a'); 
 
+			// Iteration from the last position 
+			// in the array 
+			for (int i = n - 1; i >= 0; i--) { 
+
+				k -= i; 
+
+				// If k is a positive integer 
+				if (k >= 0) { 
+
+					// 'z' needs to be inserted 
+					if (k >= 26) { 
+						arr[i] = 'z'; 
+						k -= 26; 
+					} 
+
+					// Add the required character 
+					else { 
+						arr[i] = (char)(k + 97 - 1); 
+						k -= arr[i] - 'a' + 1; 
+					} 
+				} 
+
+				else
+					break; 
+
+				k += i; 
+			} 
+
+			return new String(arr); 
+		} 
+
+		// Driver code 
+		public static void main(String[] args) 
+		{ 
+			int n = 5, k = 42; 
+
+			//char arr[] = lexo_small(n, k); 
+			String ss = lexo_small(k, n);
+			System.out.print(ss);
+			//System.out.print(new String(arr)); 
+		} 
+	} 
 
 
